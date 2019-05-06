749a542a74bfb94bd9b378fffa8b23a9c2bd1847
Kiselevich Andrey <ai_kiselevich@protek.ru>
2019-05-06 11:04:54 +0300

    Uploading files to the server and adding static styles

--- src/main/java/com/example/sweater/controller/UserController.java
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -24,6 +24,7 @@ public class UserController {
     @GetMapping
     public String userList(Model model) {
         model.addAttribute("users", userRepo.findAll());
+
         return "userList";
     }
 
@@ -31,6 +32,7 @@ public class UserController {
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
+
         return "userEdit";
     }
 
